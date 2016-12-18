3c320b6e31de4094aa1760263037d9c88c7dc38e
Bastien ALBANET <baalbane@e1r7p14.42.fr>
2016-12-18 17:13:47 +0100

    broken label

--- asm/src/check.c
+++ asm/src/check.c
@@ -6,7 +6,7 @@
 /*   By: baalbane <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/09 16:46:06 by baalbane          #+#    #+#             */
-/*   Updated: 2016/12/09 17:48:03 by baalbane         ###   ########.fr       */
+/*   Updated: 2016/12/18 17:03:00 by baalbane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,58 +15,77 @@
 int	check_all_label(t_label *label, t_op *optab)
 {
 	t_function *tmp;
-
+	printf("\nNew label: %s\n", label->name);
 	tmp = label->function;
 	while (tmp != NULL)
 	{
-		if (check_all_function(tmp, optab) == 0)
+		if (check_all_function(tmp, optab, label) == 0)
 			return (0);
 		tmp = tmp->next;
 	}
+	printf("OK!\n");
 	return (1);
 }
 
-int	check_all_function(t_function *function, t_op *optab)
+int	check_all_function(t_function *function, t_op *optab, t_label *label)
 {
 	int i;
 	t_op *tmp;
+	printf("SEARCH TAB: %s\n", function->line);
 	if ((tmp = get_good_optab(function, optab, &i)) == NULL)
 		return (0); //ERROR WRONG NAME FUNCTION
-	printf("Found optabname = %s\nfor i : %d\n", tmp->name, i);
-	if (check_arg(function->line, tmp, optab, &i, 0) == 0)
+	printf("Found optabname = %s | for i : %d\n", tmp->name, i);
+	if (check_arg(function->line, tmp, label, &i, 0) == 0)
 		return (0); //ERROR IN ARG
 	return (1);
 }
 
-int	check_arg(char *line, t_op *actualop, t_op *optab, int *i, int nbrarg)
+int	check_arg(char *line, t_op *actualop, t_label *label, int *i, int nbrarg)
 {
+	printf("Strat check arg %d\n", nbrarg);
 	while (ft_isspace(line[*i]))
-		*i = *i + 1;
+		(*i)++;
+	if (nbrarg != 0 && line[*i] != ',')
+		return (0);
 	if (line[*i] == 'r')
 	{
+		printf("\tcheck reg...");
 		if (actualop->args_type[nbrarg] % 5 != 0
 			|| check_registre(line, i) == 0)
 			return (0); //WRONG REGISTRE
+		printf("reg ok!\n");
 	}
 	else if (line[*i] == '%' && line[*i + 1] == ':')
 	{
+		printf("\tcheck T_DIR 1...");
 		if (actualop->args_type[nbrarg] % 2 != 0
-		 || check_label(line, i, optab) == 0)
+		 || check_label(line, i, label) == 0)
 			return (0);//WRONG LABEL
+		printf("T_DIR 1 ok!\n");
 	}
 	else if (line[*i] == '%')
 	{
+		printf("\tcheck T_DIR 2...");
 		if (actualop->args_type[nbrarg] % 2 != 0
 		 || check_nbr(line, i) == 0)
 			return (0);//WRONG NUMBER
-
+		printf("T_DIR 2 ok!\n");
 	}
-	else if (actualop->args_type[nbrarg] % 3 != 0
+	else
+	{
+		printf("\tcheck T_IND ...");
+		if (actualop->args_type[nbrarg] % 3 != 0
 		|| check_nbr2(line, i) == 0)
-		return (0); //WRONG NUMBER
+			return (0); //WRONG NUMBER
+		printf("T_IND ok!\n");
+	}
 	if (nbrarg+1 < actualop->args_number)
-		return (check_arg(line, actalop, optab, i, nbrarg+1));
-	return (1);
+		return (check_arg(line, actualop, label, i, nbrarg+1));
+	while (ft_isspace(line[*i]))
+		*i = *i + 1;
+	if (line[*i] == '\0')
+		return (1);
+	return (0);
 }
 
 t_op *get_good_optab(t_function *function, t_op *optab, int *i)
---
+++ asm/src/check2.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check2.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baalbane <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/12/16 16:44:07 by baalbane          #+#    #+#             */
+/*   Updated: 2016/12/18 17:13:13 by baalbane         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "asm.h"
+
+int check_registre(char *line, int *i)
+{
+	int nb = 0;
+	(*i)++;
+	while (line[*i] < '9' && line[*i] > '0')
+	{
+		nb = nb * 10 + line[*i];
+		(*i)++;
+	}
+	if (nb > REG_NUMBER)
+		return (0);
+	return (1);
+}
+
+int check_label(char *line, int *i, t_label *label)
+{
+	int j;//BROKEN
+
+	(*i) += 2;
+	printf("Search LABEL: %s starting at %d, %c\n", line, *i, line[*i]);
+	while (label != NULL)
+	{
+		printf("try LABEL: %s\n", label->name);
+		j = 0;
+		while (label->name[j] != '\0' && line[*i + j] == label->name[j])
+			j++;
+		if (label->name[j] == '\0' && line[*i + j] == ' ')
+			return (1);
+		printf("LABEL FAIL: j: %d\n", j);
+		label = label->next;
+	}
+	return (0);
+}
+
+int check_nbr(char *line, int *i)
+{
+	if (line && i)
+		return (1);
+	return (1);
+}
+
+int check_nbr2(char *line, int *i)
+{
+	if (line && i)
+		return (1);
+	return (1);
+}
